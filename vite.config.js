47017d5f73afd8eeeca48416d4e5501f410f3659
Beauty <153991439+BeautyBechara3@users.noreply.github.com>
2025-05-01 03:45:17 +0530

    all

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
-  base: "/health-tracker-frontend"
-
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
